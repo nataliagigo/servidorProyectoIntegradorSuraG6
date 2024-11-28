5f0c28a22faca9d94672e2a551ab5d38de560445
jaider-sudo <jaiderfgr22@gmail.com>
2024-11-28 15:17:01 -0500

    Se creo un api sencilla

---
+++ src/main/java/com/example/ServidorSura/ServidorSuraApplication.java
@@ -0,0 +1,14 @@
+package com.example.ServidorSura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServidorSuraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServidorSuraApplication.class, args);
+
+	}
+
+}
---
+++ src/main/java/com/example/ServidorSura/modelos/Vehiculo.java
@@ -0,0 +1,86 @@
+package com.example.ServidorSura.modelos;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "vehiculos")
+public class Vehiculo {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+        private String placa;
+        private String modelo;
+        private String cilindraje;
+        private String color;
+        private String descripcion;
+        private Integer numerosIniestros;
+
+    public Vehiculo() {
+    }
+
+    public Vehiculo(Long id, String placa, String modelo, String cilindraje, String color, String descripcion, Integer numerosIniestros) {
+        this.id = id;
+        this.placa = placa;
+        this.modelo = modelo;
+        this.cilindraje = cilindraje;
+        this.color = color;
+        this.descripcion = descripcion;
+        this.numerosIniestros = numerosIniestros;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCilindraje() {
+        return cilindraje;
+    }
+
+    public void setCilindraje(String cilindraje) {
+        this.cilindraje = cilindraje;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Integer getNumerosIniestros() {
+        return numerosIniestros;
+    }
+
+    public void setNumerosIniestros(Integer numerosIniestros) {
+        this.numerosIniestros = numerosIniestros;
+    }
+}
